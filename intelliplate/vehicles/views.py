4595a8fa1b7ee9b3e01f15272215e7870f5903b9
rajuyadav-cs <rajuyadav242625@gmail.com>
2026-06-20 04:52:06 +0530

    final touch

--- intelliplate/vehicles/views.py
+++ intelliplate/vehicles/views.py
@@ -1,8 +1,4 @@
-from datetime import datetime, time, timedelta
-
-from django.utils import timezone
 from django.views.generic import ListView, DetailView
-
 from .models import VehicleLog
 
 
@@ -15,27 +11,14 @@ class VehicleHistoryView(ListView):
     def get_queryset(self):
         queryset = VehicleLog.objects.all().order_by("-detected_at")
 
-        search = self.request.GET.get("search", "").strip()
-        selected_date = self.request.GET.get("date", "").strip()
+        search = self.request.GET.get("search")
+        date = self.request.GET.get("date")
 
         if search:
             queryset = queryset.filter(plate_number__icontains=search)
 
-        if selected_date:
-            date_obj = datetime.strptime(selected_date, "%Y-%m-%d").date()
-
-            start_datetime = timezone.make_aware(
-                datetime.combine(date_obj, time.min)
-            )
-
-            end_datetime = timezone.make_aware(
-                datetime.combine(date_obj + timedelta(days=1), time.min)
-            )
-
-            queryset = queryset.filter(
-                detected_at__gte=start_datetime,
-                detected_at__lt=end_datetime
-            )
+        if date:
+            queryset = queryset.filter(detected_at__date=date)
 
         return queryset
 
